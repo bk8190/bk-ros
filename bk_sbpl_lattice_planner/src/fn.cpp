47051b974613a69c9112e0127c11181969ba7619
Bill Kulp <bk8190@gmail.com>
2012-02-12 17:22:26 -0500

    Generates path segments - not tested

--- bk_sbpl_lattice_planner/src/fn.cpp
+++ bk_sbpl_lattice_planner/src/fn.cpp
@@ -39,32 +39,97 @@ void ConvertStateIDPathintoXYThetaPath(const EnvironmentNAVXYTHETALAT& env, cons
 
 
 // Returns a path segment between two points (x,y,theta)
+// Note: initializes all speed/accel limits to 0
 PathSegment makePathSegment(double x1, double y1, double t1, double x2, double y2, double t2)
 {
-	PathSegment seg;	
+	PathSegment seg;
 	double dx = x2-x1;
 	double dy = y2-y1;
 	double dth = t2-t1;
-	double eps = .001;
+	double eps = .001; // Precision for floating point comparison
+	
+	// Arc parameters
+	double chord_length, denom, signed_r, abs_r, ang_to_center, xcenter, ycenter;
+	
+	// Initialize the segment, set all speeds/accels to 0
+	seg.max_speeds.linear.x  = 0;
+	seg.max_speeds.angular.z = 0;
+	seg.min_speeds.linear.x  = 0;
+	seg.min_speeds.angular.z = 0;
+	seg.accel_limit          = 0;
+	seg.decel_limit          = 0;
 	
 	// No change in theta: line segment
-	if(dth == 0)
+	if(abs(dth) < eps)
 	{
-		seg.seg_type = PathSegment::LINE;
-		seg.seg_length = sqrt(dx*dx + dy*dy);
-		seg.ref_point = geometry_msgs::Point(x1,y1);
-		seg.init_tan_angle = tf::M
-		seg.curvature = 0.0;
+		seg.seg_type       = precision_navigation_msgs::PathSegment::LINE;
+		seg.seg_length     = sqrt(dx*dx + dy*dy);
+		seg.ref_point.x    = x1;
+		seg.ref_point.y    = y1;
+		seg.ref_point.z    = 0.0;
+		seg.init_tan_angle = tf::createQuaternionMsgFromYaw(t1);
+		seg.curvature      = 0.0;
 	}
 	// No change in position: turn in place
 	else if( abs(dx)<eps && abs(dy)<eps )
 	{
-		seg.seg_type = PathSegment::SPIN_IN_PLACE
-	
+		seg.seg_type       = precision_navigation_msgs::PathSegment::SPIN_IN_PLACE
+		seg.seg_length     = abs(dth);
+		seg.ref_point.x    = x1;
+		seg.ref_point.y    = y1;
+		seg.ref_point.z    = 0.0;
+		seg.init_tan_angle = tf::createQuaternionMsgFromYaw(t1);
+		
+		// Positive curvature -> CW rotation, negative curvature -> CCW rotation
+		if( dth>0 )
+			seg.curvature =  1.0;
+		else
+			seg.curvature = -1.0;
 	}
 	// Else: arc segment
 	else
 	{
-		seg.seg_type = PathSegment::ARC;
+		seg.seg_type = precision_navigation_msgs::PathSegment::ARC;
+		seg.init_tan_angle = tf::createQuaternionMsgFromYaw(t1);
+		
+		// Use the formula chord_length = 2*radius*sin(dtheta) -> radius = chord_length/(2*sin(dtheta))
+		// Note that from this formula, r can be either positive or negative.
+		// Positive r: segment bending to the right
+		// Negative r: segment bending to the left
+		chord_length = sqrt(dx*dx + dy*dy);
+		denom = 2*sin(dth);
+		if(denom != 0)
+		{
+			signed_r = chord_length / (denom);
+			abs_r    = abs(signed_r); // Absolute, positive radius
+			
+			if(signed_r != 0)		
+				seg.curvature = 1/signed_r;
+			else{
+				seg.curvature = 0;
+				ROS_ERROR("Invalid arc segment created!");
+			}
+		}
+		else{
+			seg.curvature = 0;
+			ROS_ERROR("Invalid arc segment created!");
+		}
+		
+		// Find the angle to the circle's center from the starting point
+		if( dth>0 )
+			ang_to_center = t1 + PI/2; // 90 degrees to the right
+		if( dth<0 )
+			ang_to_center = t1 - PI/2; // 90 degrees to the left
+		
+		// Move to the circle's center
+		xcenter = x1 + abs_r*cos(ang_to_center);
+		ycenter = y1 + abs_r*sin(ang_to_center);
+		
+		seg.ref_point.x = xcenter;
+		seg.ref_point.y = ycenter;
+		seg.ref_point.z = 0.0;
+		
+		// Arc length = r*theta
+		seg.seg_length = abs_r * dth;
 	}
 }
