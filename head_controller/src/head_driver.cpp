cf136899deb17fed0001c52f25913dec561c166f
Bill Kulp <bk8190@gmail.com>
2012-03-03 19:09:31 -0500

    Fixed bugs

--- head_controller/src/head_driver.cpp
+++ head_controller/src/head_driver.cpp
@@ -1,16 +1,16 @@
 #include <ros/ros.h>
 #include <tf/tf.h>
+#include <std_msgs/Float64.h>
 #include <tf/transform_broadcaster.h>
 
-double des_pan_;
+double des_pan_ = 0.0;
 
 void panAngleCallback(const std_msgs::Float64& msg)
 {
 	des_pan_ = msg.data;
 }
 
-int
-main(int argc, char** argv)
+int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "head_driver");
 	ros::NodeHandle nh;
@@ -28,7 +28,7 @@ main(int argc, char** argv)
 	// We control a servo that determines the link between parent_frame and child_frame
 	std::string parent_frame, child_frame;
 	nh.param<std::string>("parent_tf_frame", parent_frame , "NULL");
-	nh.param<std::string>("pan_tf_frame"   , child_frame  , "NULL");
+	nh.param<std::string>("child_tf_frame" , child_frame  , "NULL");
 	ROS_INFO("[head_driver] Broadcasting TF from \"%s\" to \"%s\"",
 		parent_frame.c_str(), child_frame.c_str());
 	
@@ -50,7 +50,7 @@ main(int argc, char** argv)
 		tf::Transform transform;
 		transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0) );
 		transform.setRotation( tf::Quaternion(cmd_pan, 0.0, 0.0) );
-		br.sendTransform( tf::StampedTransform(transform, ros::Time::now(), parent_frame, child_frame );
+		br.sendTransform( tf::StampedTransform(transform, ros::Time::now(), parent_frame, child_frame ));
 	
 		// Allow callbacks to occur, and sleep to enforce the desired rate.
 		ros::spinOnce();
@@ -58,5 +58,5 @@ main(int argc, char** argv)
 	}
 	
 	ROS_INFO("[head_driver] Shutdown now.");
-	return(0);
+	return 0;
 }
